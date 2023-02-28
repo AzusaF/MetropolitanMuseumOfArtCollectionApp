332f6e54ba81f9b032f5c2b2af2178f415758f23
Azusa Fukuda <afukuda@mySeneca.ca>
2023-02-28 13:25:56 -0500

    done Navbar

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap';
+import MainNav from './MainNav';
+
+export default function Layout(props) {
+   return (
+     <>
+      <MainNav/> <br /> 
+      <Container>
+         {props.children} 
+      </Container>
+      <br />
+     </>
+   );
+}
---
+++ components/MainNav.js
@@ -0,0 +1,48 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+function MainNav() {
+
+   const router = useRouter()
+   const [title, setTitle] = useState('');
+
+   function submitForm(e) {
+      e.preventDefault(); // prevent the browser from automatically submitting the form
+      console.log(`form submitted - title: ${title}`);
+   }
+  
+   return (
+   <>
+   <Navbar bg="light" expand="lg"className='fixed-top navbar-light bg_primary'>
+      <Container>
+        <Navbar.Brand>Azusa Fukuda</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/" passHref legacyBehavior>Home</Nav.Link>
+            <Nav.Link href="/search" passHref legacyBehavior>Advanced Search</Nav.Link>
+            <Form onSubmit={submitForm} className="d-flex">
+               <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                  value={title}
+                  onChange={(e)=>setTitle(e.target.value)}
+               />
+               <Button variant="outline-success" onClick={()=>router.push(`/artwork?title=true&q=${title}`)} >Search</Button>
+            </Form>
+          </Nav>
+      </Container>
+   </Navbar>
+   <br/>
+   <br/>
+   </>
+  );
+}
+
+export default MainNav;
