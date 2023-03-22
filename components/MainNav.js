43996fb762d0eaefa0034b0d0673c9a0a8453230
Azusa Fukuda <afukuda@mySeneca.ca>
2023-03-21 21:34:59 -0400

    solved the Navbar  problem

--- components/MainNav.js
+++ components/MainNav.js
@@ -12,36 +12,43 @@ export default function MainNav() {
 
    const router = useRouter();
    const [title, setTitle] = useState('');
+   const [isExpanded, setIsExpanded] = useState(false);
 
 
    function submitForm(e) {
       e.preventDefault(); // prevent the browser from automatically submitting the form
       // console.log(`form submitted - title: ${title}`);
       router.push(`/artwork?title=true&q=${title}`);
+      setIsExpanded(false);
    }
   
    return (
    <>
-   <Navbar bg="light" expand="lg" className='fixed-top navbar-light bg_primary'>
+   <Navbar expanded={isExpanded} bg="light" expand="lg" className='fixed-top navbar-light bg_primary'>
       <Container>
-         <Nav>
-            <Navbar.Brand>Azusa Fukuda</Navbar.Brand>
-            <Link passHref legacyBehavior href="/" ><Nav.Link >Home</Nav.Link></Link>
-            <Link passHref legacyBehavior href="/search" ><Nav.Link >Advanced Search</Nav.Link></Link>
-         </Nav>
-         <Nav>      
-            <Form onSubmit={submitForm} className="d-flex">
-            <Form.Control
-               type="search"
-               placeholder="Search"
-               className="me-2"
-               aria-label="Search"
-               value={title}
-               onChange={(e)=>setTitle(e.target.value)}
-            />
-            <Button variant="outline-success" type="submit" >Search</Button>
-            </Form>
-          </Nav>
+         <Navbar.Brand>Azusa Fukuda</Navbar.Brand>
+         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={()=> setIsExpanded(!isExpanded)} />
+         <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+               <Link passHref legacyBehavior href="/" ><Nav.Link onClick={()=>setIsExpanded(false)}>Home</Nav.Link></Link>
+               <Link passHref legacyBehavior href="/search" ><Nav.Link onClick={()=> setIsExpanded(false)}>Advanced Search</Nav.Link></Link>
+            </Nav>
+            <Nav>
+               &nbsp;  
+               <Form onSubmit={submitForm} className="d-flex">
+               <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                  value={title}
+                  onChange={(e)=>setTitle(e.target.value)}
+               />
+               <Button variant="outline-success" type="submit" >Search</Button>
+               </Form>
+               &nbsp;
+            </Nav>
+         </Navbar.Collapse>
       </Container>
    </Navbar>
    <br/>
