1e50f3670cdd1b011834610459b834faf5c8f74f
Azusa Fukuda <afukuda@mySeneca.ca>
2023-04-05 12:42:37 -0400

    added login.js

---
+++ pages/login.js
@@ -0,0 +1,56 @@
+import { Card, Form, Button, Alert } from "react-bootstrap";
+import {useState} from 'react';
+import { authenticateUser } from "@/lib/authenticate";
+import { useRouter } from "next/router";
+import { searchHistoryAtom, favouritesAtom } from "@/store";
+import { useAtom } from "jotai";
+import { getFavourites, getHistory } from "@/lib/userData";
+
+export default function Login(props){
+
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
+  const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+
+  const router = useRouter();
+
+   async function updateAtoms(){
+      setFavouritesList(await getFavourites());
+      setSearchHistory(await getHistory());
+   }
+
+   function handleSubmit(e){
+      e.preventDefault();
+      // console.log(userName, password);
+      authenticateUser(userName,password).then(async()=>{
+      // console.log("valid user")
+         await updateAtoms();
+         router.push("/favourites")
+      }).catch(err=>{
+         setMessage(err.message);
+      })
+   }
+
+  return (
+    <>
+      <Card bg="light">
+        <Card.Body><h2>Login</h2>Enter your login information below:</Card.Body>
+      </Card>
+      <br />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>User:</Form.Label><Form.Control value={userName} onChange={e => setUserName(e.target.value)} type="text" id="userName" name="userName" />
+        </Form.Group>
+        <br />
+        <Form.Group>
+          <Form.Label>Password:</Form.Label><Form.Control value={password} onChange={e=>setPassword(e.target.value)} type="password" id="password" name="password" />
+        </Form.Group>
+        <br />
+        <Button variant="primary" className="pull-right" type="submit">Login</Button>
+      </Form>
+      { message && ( <><br /><Alert variant="danger">{message}</Alert></> )}
+    </>
+  );
+}
