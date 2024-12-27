05b8fa7b82205d13ad0c6c80fdfa8ae9099a6f0d
Azusa Fukuda <afukuda@mySeneca.ca>
2024-12-27 14:42:17 -0500

    add google site verification

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="google-site-verification" content="F9AncBJtL7TJ-9jJuWFyAXwkXxKl5PvoY6foc4FtGYU" />
       <body>
         <Main />
         <NextScript />
